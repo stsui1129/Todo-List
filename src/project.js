160fb518adde96a190ee29d2977f480e2971ccf3
Sheng <s.tsui@outlook.com>
2021-03-23 21:25:58 +1300

    brainstorm project

---
+++ src/project.js
@@ -0,0 +1,24 @@
+class Project {
+    constructor(title) {
+        this.title = title;
+        this.toDo = [];
+    }
+
+    set title(newTitle) {
+        this.title = newTitle;
+    }
+
+    addToDo(title, description, dueDate, priority) {
+        const toDo = new toDO(title, description, dueDate, priority)
+        this.toDo.push(toDo);
+    }
+}
+
+class ToDo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
