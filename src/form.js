a0a79e489a7f69c87903050e700ea72df7f4e398
Sheng <s.tsui@outlook.com>
2021-03-29 21:21:30 +1300

    added form DOM

--- src/form.js
+++ src/form.js
@@ -1,1 +1,9 @@
 const modal = document.querySelector('.modal');
+const trigger = document.querySelector('.trigger');
+trigger.addEventListener("click", toggleModal);
+
+const closeButton = document.querySelector(".close-button");
+closeButton.addEventListener("click", toggleModal);
+
+const cancelButton = document.querySelector("#cancel-button");
+cancelButton.addEventListener("click", toggleModal);
