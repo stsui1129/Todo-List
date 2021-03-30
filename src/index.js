f526eaa278a389654b7290b3b9250b12c6e607dc
Sheng <s.tsui@outlook.com>
2021-03-30 20:06:09 +1300

    updated modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("testing");
+const modal = document.querySelector('.modal');
+const trigger = document.querySelector('.trigger');
+trigger.addEventListener("click", toggleModal);
+
+const closeButton = document.querySelector(".close-button");
+closeButton.addEventListener("click", toggleModal);
+
+const cancelButton = document.querySelector("#cancel-button");
+cancelButton.addEventListener("click", toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle("show-modal"); //toggles between .modal class and .show-modal class
+}
+
